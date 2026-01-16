1db8446b21d3c4554e1fa0de2dd1445b9db4ee0a
Arnav Sacheti <36746504+arnavsacheti@users.noreply.github.com>
2026-01-16 14:25:11 +0000

    Update to use parameter instead of hardcoded value everywhere

--- src/peakrdl_busdecoder/cpuif/fanin_gen.py
+++ src/peakrdl_busdecoder/cpuif/fanin_gen.py
@@ -39,11 +39,13 @@ class FaninGenerator(BusDecoderListener):
             return action
 
         if node.array_dimensions:
-            for i, dim in enumerate(node.array_dimensions):
+            for i in range(len(node.array_dimensions)):
                 fb = ForLoopBody(
                     "int",
                     f"i{i}",
-                    dim,
+                    f"N_{node.inst_name.upper()}S_{i}"
+                    if len(node.array_dimensions) > 1
+                    else f"N_{node.inst_name.upper()}S",
                 )
                 self._stack.append(fb)
 
--- src/peakrdl_busdecoder/cpuif/fanin_intermediate_gen.py
+++ src/peakrdl_busdecoder/cpuif/fanin_intermediate_gen.py
@@ -45,11 +45,13 @@ class FaninIntermediateGenerator(BusDecoderListener):
 
         # Generate assignment logic using generate loops
         if node.array_dimensions:
-            for i, dim in enumerate(node.array_dimensions):
+            for i in range(len(node.array_dimensions)):
                 fb = ForLoopBody(
                     "genvar",
                     f"gi{i}",
-                    dim,
+                    f"N_{node.inst_name.upper()}S_{i}"
+                    if len(node.array_dimensions) > 1
+                    else f"N_{node.inst_name.upper()}S",
                 )
                 self._stack.append(fb)
 
